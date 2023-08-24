e39e602f88b8beac9f238fb0c38b34f555e7cc5c
Suganya <suganya.pasm@gmail.com>
2023-08-24 09:36:41 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -24,7 +24,8 @@ resource "azurerm_resource_group" "myresourcegroup" {
 
   tags = {
     environment = "Production"
-    Department = devops
+    Department = "devops"
+    Billable = true
   }
 }
 
